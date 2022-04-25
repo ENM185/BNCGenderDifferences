f811cf102af52136ba895a4f71008d3e4cbb69d5
ENM185 <ericnathanmiller@icloud.com>
2022-04-25 10:30:23 -0400

    Improve alignment to include entire vocabulary of both corpora

--- embedding/align.py
+++ embedding/align.py
@@ -7,26 +7,16 @@ female_model = word2vec.Word2Vec.load("original/female.model")
 # Intersect vocabularies
 shared_words = list(set(male_model.wv.index_to_key) & set(female_model.wv.index_to_key))
 
-word_to_shared_idx = {}
-for i, word in enumerate(shared_words):
-    word_to_shared_idx[word] = i
+male_shared_indices = [male_model.wv.key_to_index[w] for w in shared_words]
+male_vectors = np.array([male_model.wv.vectors[i] for i in male_shared_indices])
 
-new_male_indices = [male_model.wv.key_to_index[w] for w in shared_words]
-male_model.wv.vectors = np.array([male_model.wv.vectors[i] for i in new_male_indices])
-
-new_female_indices = [female_model.wv.key_to_index[w] for w in shared_words]
-female_model.wv.vectors = np.array([female_model.wv.vectors[i] for i in new_female_indices])
-
-male_model.wv.index_to_key = shared_words
-female_model.wv.index_to_key = shared_words
-
-male_model.wv.key_to_index = word_to_shared_idx
-female_model.wv.key_to_index = word_to_shared_idx
+female_shared_indices = [female_model.wv.key_to_index[w] for w in shared_words]
+female_vectors = np.array([female_model.wv.vectors[i] for i in female_shared_indices])
 
 # Procrustes alignment
-M = (female_model.wv.vectors).T.dot(male_model.wv.vectors) 
+M = (female_vectors).T.dot(male_vectors) 
 U, _, V = np.linalg.svd(M)
-R = U.dot(V) 
+R = U.dot(V)
 female_model.wv.vectors = (female_model.wv.vectors).dot(R)    
 
 male_model.save("aligned/male.model")
