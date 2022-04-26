d74a10133aace9c7205e1b7fa6323a70156f025a
ENM185 <ericnathanmiller@icloud.com>
2022-04-26 10:29:47 -0400

    Restrict cooccurrence to sentences

--- cooccurrences.R
+++ cooccurrences.R
@@ -5,5 +5,8 @@ spoken_male <- partition("BNC", u_sex="male")
 spoken_female <- partition("BNC", u_sex="female")
 
 word <- "ball_SUBST"
-male_co <- cooccurrences(spoken_male, query = paste("[lemma='", word, "']", sep=""), left = 8, right=8, method="chisquare", verbose=T, cqp=T, p_attribute="lemma")
-female_co <- cooccurrences(spoken_female, query = paste("[lemma='", word, "']", sep=""), left = 8, right=8, method="chisquare", verbose=T, cqp=T, p_attribute="lemma")
+male_co <- cooccurrences(spoken_male, query = paste("[lemma='", word, "']", sep=""), left = 8, right=8, method="chisquare", verbose=T, cqp=T, p_attribute="lemma", boundary="s")
+female_co <- cooccurrences(spoken_female, query = paste("[lemma='", word, "']", sep=""), left = 8, right=8, method="chisquare", verbose=T, cqp=T, p_attribute="lemma", boundary="s")
+
+male <- as.data.frame(male_co)
+female <- as.data.frame(female_co)
